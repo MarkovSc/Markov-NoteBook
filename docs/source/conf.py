b6a519001c56411186c5101029590f48919938e4
Markov <2412546652@qq.com>
2023-08-28 19:29:28 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,8 +3,8 @@
 # -- Project information
 
 project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+copyright = '2023, markov'
+author = 'markov_engineer@163.com'
 
 release = '0.1'
 version = '0.1.0'
