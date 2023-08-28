8a30cb5d7449064d688620e2fb95d8d3d636927d
Markov <2412546652@qq.com>
2023-08-28 19:03:43 +0800

    Author

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Markov算法笔记'
+copyright = '2023, markov'
+author = 'markov_engineer@163.com'
 
 release = '0.1'
 version = '0.1.0'
